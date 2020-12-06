7eb3051599cca83a484c673ae8014f79514cf72a
pxseu <57842793+pxseu@users.noreply.github.com>
2020-12-06 19:32:26 +0100

    better_404

--- js/404.js
+++ js/404.js
@@ -1,6 +1,7 @@
 const pathNotFoundEl = document.getElementById("pathNotFound");
 
 window.onload = () => {
-	const currentPath = window.location.pathname;
+	let currentPath = window.location.pathname;
+	currentPath = currentPath.substring(1);
 	pathNotFoundEl.innerText = decodeURIComponent(currentPath);
 };
