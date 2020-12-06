dcfec54058e5b0aed2631fc57e5494b149e2c6be
pxseu <57842793+pxseu@users.noreply.github.com>
2020-12-06 12:13:59 +0100

    forgot about cool urls

--- js/404.js
+++ js/404.js
@@ -2,5 +2,5 @@ const pathNotFoundEl = document.getElementById("pathNotFound");
 
 window.onload = () => {
 	const currentPath = window.location.pathname;
-	pathNotFoundEl.innerText = currentPath;
+	pathNotFoundEl.innerText = decodeURIComponent(currentPath);
 };
